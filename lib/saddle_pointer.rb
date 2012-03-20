0b677e2dfd469804a55d91957d1c983950908511
William Dix <william.j.dix@gmail.com>
2012-03-20 07:30:04 -0500

    col and row candidates found correctly

--- lib/saddle_pointer.rb
+++ lib/saddle_pointer.rb
@@ -1,8 +1,46 @@
 require "saddle_pointer/version"
 
 module SaddlePointer
-  class << self
-    def find(array)
+  class Point
+    attr_reader :x, :y
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+    def ==(other)
+      other.x == self.x && 
+        other.y == self.y
     end
   end
+
+
+  class << self
+    def find array
+    end
+    def find_row_candidates array
+      candidates = []
+      array.each_with_index do |row, row_idx|
+        max = row.max
+        row.each_with_index do |item, col_idx|
+          if item == max
+            candidates << Point.new(col_idx, row_idx)
+          end
+        end
+      end
+      candidates
+    end
+
+    def find_column_candidates array
+      candidates = []
+      array.transpose.each_with_index do |column, col_idx|
+        min = column.min
+        column.each_with_index do |item, row_idx|
+          if item == min
+            candidates << Point.new(col_idx, row_idx)
+          end
+        end
+      end
+      candidates
+    end
+  end
+
 end
--- test/saddle_pointer_test.rb
+++ test/saddle_pointer_test.rb
@@ -1,28 +1,64 @@
 require_relative './test_helper'
 require_relative '../lib/saddle_pointer'
 
-class SaddlePointerTest < MiniTest::Unit::TestCase
-  def test_the_sample_arrays
-    arr1 = [[39, 43, 49, 29, 18],
-            [30, 47, 24, 29, 15], 
-            [49, 50, 39, 20, 33],
-            [18, 38, 35, 32, 35],
-            [29, 44, 18, 34, 44]]
+module SaddlePointer
+  class SaddlePointerTest < MiniTest::Unit::TestCase
+    def test_the_sample_arrays
+      arr1 = [[39, 43, 49, 29, 18],
+        [30, 47, 24, 29, 15], 
+        [49, 50, 39, 20, 33],
+        [18, 38, 35, 32, 35],
+        [29, 44, 18, 34, 44]]
 
-    arr2 = [[50, 27, 36, 43, 39],
-            [36, 14, 35, 40, 19],
-            [20, 33, 48, 32, 40],
-            [41, 40, 15, 22, 19],
-            [21, 24, 24, 31, 18]]
+      arr2 = [[50, 27, 36, 43, 39],
+        [36, 14, 35, 40, 19],
+        [20, 33, 48, 32, 40],
+        [41, 40, 15, 22, 19],
+        [21, 24, 24, 31, 18]]
 
-    arr3 = [[39, 43, 49, 29, 18], 
-            [30, 47, 24, 29, 15], 
-            [49, 50, 39, 20, 33],
-            [18, 38, 35, 32, 38], 
-            [29, 44, 18, 34, 44]]
+      arr3 = [[39, 43, 49, 29, 18], 
+        [30, 47, 24, 29, 15], 
+        [49, 50, 39, 20, 33],
+        [18, 38, 35, 32, 38], 
+        [29, 44, 18, 34, 44]]
 
-    assert_equal [[3, 1]], SaddlePointer.find(arr1)
-    assert_equal "No saddle points found", SaddlePointer.find(arr2)
-    assert_equal [[3,1]], SaddlePointer.find(arr3)
+      assert_equal [Point.new(3, 1)], SaddlePointer.find(arr1)
+      assert_equal "No saddle points found", SaddlePointer.find(arr2)
+      assert_equal [Point.new(3,1)], SaddlePointer.find(arr3)
+    end
+
+    def test_find_saddle_points_by_intersecting_row_and_column_candidates
+
+    end
+
+    def test_finding_row_candidates
+      arr  = [[39, 43, 49, 29, 18], 
+              [30, 47, 24, 29, 15], 
+              [49, 50, 39, 20, 33],
+              [18, 38, 35, 32, 38], 
+              [29, 44, 18, 34, 44]]
+      found = SaddlePointer.find_row_candidates(arr)
+      assert_includes found, Point.new(2,0)
+      assert_includes found, Point.new(1,1)
+      assert_includes found, Point.new(1,2)
+      assert_includes found, Point.new(1,3)
+      assert_includes found, Point.new(4,3)
+      assert_includes found, Point.new(1,4)
+      assert_includes found, Point.new(4,4)
+    end
+
+    def test_finding_column_candidates
+      arr  = [[39, 43, 49, 29, 18], 
+              [30, 47, 24, 29, 15], 
+              [49, 50, 39, 20, 33],
+              [18, 38, 35, 32, 38], 
+              [29, 44, 18, 34, 44]]
+      found = SaddlePointer.find_column_candidates arr
+      assert_includes found, Point.new(0,3)
+      assert_includes found, Point.new(1,3)
+      assert_includes found, Point.new(2,4)
+      assert_includes found, Point.new(3,2)
+      assert_includes found, Point.new(4,1)
+    end
   end
 end
